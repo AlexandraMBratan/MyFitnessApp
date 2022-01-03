be5dd64cd85c6f914df4ce5c71dd9e39c3e5c5b4
AlexandraMBratan <maria.bratan@student.upt.ro>
2022-01-03 22:12:42 +0200

    Firebase connection & create users

---
+++ app/src/main/java/com/example/myfitnessapp/User.java
@@ -0,0 +1,19 @@
+package com.example.myfitnessapp;
+
+public class User {
+    public String name,surname,age,height,weight,gender,email;
+
+    public User(){
+
+    }
+
+    public User(String name, String surname, String age, String height, String weight, String gender, String email){
+        this.name=name;
+        this.surname=surname;
+        this.age=age;
+        this.height=height;
+        this.weight=weight;
+        this.gender=gender;
+        this.email=email;
+    }
+}
