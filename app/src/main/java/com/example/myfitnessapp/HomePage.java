b29127e945c815aea012a1942de9f7144a82b8f2
AlexandraMBratan <maria.bratan@student.upt.ro>
2022-01-07 19:37:02 +0200

    Log Out activity

--- app/src/main/java/com/example/myfitnessapp/HomePage.java
+++ app/src/main/java/com/example/myfitnessapp/HomePage.java
@@ -2,15 +2,19 @@ package com.example.myfitnessapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 private DrawerLayout drawer;
@@ -45,10 +49,35 @@ private DrawerLayout drawer;
             case R.id.nav_home:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
                 break;
+            case R.id.nav_logout:
+                logout(this);
+                break;
         }
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    private void logout(HomePage homePage) {
+        AlertDialog.Builder builder=new AlertDialog.Builder(homePage);
+        builder.setTitle("LogOut");
+        builder.setMessage("Are you sure you want to logout?");
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                homePage.finishAffinity();
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(HomePage.this,MainActivity.class));
+            }
+        });
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+    }
+
 
     @Override
     public void onBackPressed() {
